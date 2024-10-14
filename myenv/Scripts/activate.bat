a39c843241afa4a55ac7143bd5fb9e00b54ed0c3
Gafabusa Willy <willygafabusa@gmail.com@student.mak.ac.ug>
2024-10-14 18:44:05 +0300

    added github actions

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USER\Desktop\INTERNSHIP\CODING\project\myenv
+set VIRTUAL_ENV=C:\Users\USER\Desktop\COMBINED\YR 4 SEM 1\Emerging Trends in SE\Project\STRICTLY PRACTICE\Food-Ordering-Site\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
